0d4d2df673e346b88afdf746aa56522ab5de47d4
Austinma1010 <149961201+Austinma1010@users.noreply.github.com>
2024-02-08 21:19:36 -0500

    changed route endpoint notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -2,13 +2,13 @@ const notes = require('express').Router();
 const {readAndAppend, readFromFile} = require('../utils/fsTools');
 const { v4: uuidv4 } = require('uuid');
 
-notes.get('/', (req, res) => {
+notes.get('/api/notes.js', (req, res) => {
     readFromFile('../NotePad/db/db.json').then((data) => 
     res.json(JSON.parse(data))
     );
 });
 
-notes.post('/', (req, res) => {
+notes.post('/api/notes.js', (req, res) => {
     const newId = uuidv4();
     req.body.id = newId;
     readAndAppend(req.body, '../NotePad/db/db.json');
