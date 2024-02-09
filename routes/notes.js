316558e3b2015e85b5fc4f56f506d4abc7fcb163
Austinma10 <austinma2168@gmail.com>
2024-02-08 22:20:47 -0500

    somethign

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ const {readAndAppend, readFromFile} = require('../utils/fsTools');
 const { v4: uuidv4 } = require('uuid');
 
 notes.get('/', (req, res) => {
-    readFromFile('../db/db.json').then((data) => 
+    readFromFile('../NotePad/db/db.json').then((data) => 
     res.json(JSON.parse(data))
     );
 });
@@ -11,9 +11,9 @@ notes.get('/', (req, res) => {
 notes.post('/', (req, res) => {
     const newId = uuidv4();
     req.body.id = newId;
-    readAndAppend(req.body, '../db/db.json');
+    readAndAppend(req.body, '../NotePad/db/db.json');
     console.log('added note');
-    readFromFile('../db/db.json').then((data) => 
+    readFromFile('../NotePad/db/db.json').then((data) => 
     res.json(JSON.parse(data))
     );
     
