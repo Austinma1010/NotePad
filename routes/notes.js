6e9a3419db83515069a0b318ffaf07f7e57cd647
Austinma1010 <149961201+Austinma1010@users.noreply.github.com>
2024-02-08 21:56:54 -0500

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -2,13 +2,13 @@ const notes = require('express').Router();
 const {readAndAppend, readFromFile} = require('../utils/fsTools');
 const { v4: uuidv4 } = require('uuid');
 
-notes.get('/notes', (req, res) => {
+notes.get('/', (req, res) => {
     readFromFile('../NotePad/db/db.json').then((data) => 
     res.json(JSON.parse(data))
     );
 });
 
-notes.post('/notes', (req, res) => {
+notes.post('/', (req, res) => {
     const newId = uuidv4();
     req.body.id = newId;
     readAndAppend(req.body, '../NotePad/db/db.json');
