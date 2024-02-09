159e9d8f307441c52140b1ade68077dc154baa78
Austinma1010 <149961201+Austinma1010@users.noreply.github.com>
2024-02-08 20:46:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const apiRouter = require('./routes/index.js');
+const apiRouter = require('../routes/index.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,16 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'public', 'notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('*', (req, res) => 
-res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
