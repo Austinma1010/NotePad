7a1c33c751c8dec188c3cf93d7e4129d5013e5ca
Austinma1010 <149961201+Austinma1010@users.noreply.github.com>
2024-02-08 19:58:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const apiRouter = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('NotePad'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
