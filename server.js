b4631c2274e4e64850e0af258e39daf274daed03
Austinma1010 <149961201+Austinma1010@users.noreply.github.com>
2024-02-08 20:48:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const apiRouter = require('../routes/index.js');
+const apiRouter = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
