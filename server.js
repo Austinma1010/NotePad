2c60a5d0725241361e48375f49d779b9d0720a98
Austinma1010 <149961201+Austinma1010@users.noreply.github.com>
2024-02-08 20:40:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.resolve(__dirname, '/public/index.html'))
 );
 
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.resolve(__dirname, '/public/notes.html'))
 );
 
 app.get('*', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.resolve(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
